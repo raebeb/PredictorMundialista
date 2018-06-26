cffb36440cbead13827174af3a33c49df6e01978
Raebeb <raebeb@gmail.com>
2018-06-25 22:38:06 -0400

    vistas por grupo

---
+++ app/src/main/java/com/magister/unab/predictormundialista/porcentaje_d.java
@@ -0,0 +1,13 @@
+package com.magister.unab.predictormundialista;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class porcentaje_d extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_porcentaje_d);
+    }
+}
